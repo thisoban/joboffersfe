3b6691694cbe918fab1accc9fffceec1a176b21e
thisoban <thisoban_1995@hotmail.com>
2023-10-24 20:22:31 +0200

    edited with design

--- src/Pages/Job.js
+++ src/Pages/Job.js
@@ -1,54 +1,79 @@
 import React from "react";
-import Joboffer from "../Component/joboffer";
-//put everything what the job requires
-//job title
-// job description
-// job requirement
-// job offers you
-// job hours
-// yearly pay
-// 
-function Job() {
-    return (
-        <>
-        <div className="container">
-            {/* <div className="grid"> */}
-                <div className="row">
-                    <div className="col-6">
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
-                        <p><h1> job title</h1></p>
+const job = () => {
+  return (
+    <Container className="my-4">
+      <Row>
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <h2>Software Developer</h2>
+              <p>
+                We are looking for a passionate and talented Software Developer
+                to join our team.
+              </p>
+              <h3>Job Description</h3>
+              <ul>
+                <li>
+                  Design and develop web applications using JavaScript and React
+                </li>
+                <li>
+                  Collaborate with the development team to implement new
+                  features
+                </li>
+                <li>Optimize applications for maximum performance</li>
+                <li>Write clean, maintainable, and efficient code</li>
+              </ul>
+              <h3>Requirements</h3>
+              <ul>
+                <li>Bachelor's degree in Computer Science or related field</li>
+                <li>
+                  Proficiency in JavaScript, React, and related technologies
+                </li>
+                <li>Strong problem-solving skills</li>
+                <li>Excellent communication and teamwork abilities</li>
+              </ul>
+              <Button variant="primary">Apply Now</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <h3>Hiring Manager</h3>
+              <img
+                src="manager.jpg"
+                alt="Hiring Manager"
+                className="img-fluid rounded-circle"
+              />
+              <h4>Jane Doe</h4>
+              <p>Email: janedoe@example.com</p>
+              <p>Phone: (555) 123-4567</p>
+            </Card.Body>
+          </Card>
+          <Card className="mt-4">
+            <Card.Body>
+              <h3>Company</h3>
+              <img
+                src="company_logo.png"
+                alt="Company Logo"
+                className="img-fluid"
+              />
+              <h4>Acme Software Solutions</h4>
+              <p>Location: New York, NY</p>
+              <p>
+                Website:{" "}
+                <a href="https://www.acmesoftware.com" target="_blank">
+                  www.acmesoftware.com
+                </a>
+              </p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-                        <p> <h3> job description </h3></p>
-
-                        <p>pay rate</p>
-                    </div>
-
-                    <div className="col-6">
-                        <p><h2>company name</h2> </p>
-                        <p> <h6>company small summary</h6></p>
-                        <p> <h6>company adres details</h6></p>
-
-                    </div>
-                </div>
-
-
-
-            {/* </div> */}
-
-
-        </div>
-        <div className="container"> 
-        <div className="row">
-            <div className="col-4">
-                <h3>hiring manager</h3>
-                <p> name</p>
-                <p>contact details</p>
-                <img src="test" />
-            </div>
-        </div>
-        
-        </div>
-       
-</>
-    );
-}
+export default job;
--- src/Pages/jobs.js
+++ src/Pages/jobs.js
@@ -1,11 +1,48 @@
 import React from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
-function Jobs(){
-    return(
-        <div>
-            <h1> jobs listing</h1>
-        </div>
-    );
-}
+const jobs = [
+  {
+    id: 1,
+    title: "Software Developer",
+    description:
+      "We are looking for a passionate and talented Software Developer to join our team.",
+    location: "New York, NY",
+    applyLink: "apply-software-developer",
+  },
+  {
+    id: 2,
+    title: "Frontend Developer",
+    description:
+      "Join our team as a Frontend Developer and help build amazing user interfaces.",
+    location: "San Francisco, CA",
+    applyLink: "apply-frontend-developer",
+  },
+  // Add more job listings here...
+];
 
-export default Jobs;
+const JobListings = () => {
+  return (
+    <Container className="my-4">
+      <h1>Job Listings</h1>
+      <Row>
+        {jobs.map((job) => (
+          <Col key={job.id} md={4}>
+            <Card className="mb-4">
+              <Card.Body>
+                <h2>{job.title}</h2>
+                <p>{job.description}</p>
+                <p>Location: {job.location}</p>
+                <Button variant="primary" href={"/job"}>
+                  Apply Now
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default JobListings;
