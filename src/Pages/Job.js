f2fa7550709af967b0a8293db4312c0801154172
thisoban <thisoban_1995@hotmail.com>
2023-11-06 14:59:34 +0100

    api call to normal backend without docker

--- src/Pages/Job.js
+++ src/Pages/Job.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
-const job = () => {
+
+
+export default function Job()  {
+  const [JobData, setJobData] = useState({});
+  useEffect(() => {
+    const apiUrl = 'http://localhost:4863/Jobs/details?id=1';
+
+    axios.get(apiUrl)
+      .then((response) => {
+        // Set the jobData state with the API response data
+        setJobData(response.data);
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the request
+        console.error('Error:', error);
+      });
+  }, []); // The empty dependency array means this effect will run once when the component is mounted.
+
   return (
     <Container className="my-4">
       <Row>
         <Col md={8}>
           <Card>
             <Card.Body>
-              <h2>Software Developer</h2>
+              <h2>{JobData.name}</h2>
               <p>
                 We are looking for a passionate and talented Software Developer
                 to join our team.
@@ -47,9 +65,9 @@ const job = () => {
                 alt="Hiring Manager"
                 className="img-fluid rounded-circle"
               />
-              <h4>Jane Doe</h4>
-              <p>Email: janedoe@example.com</p>
-              <p>Phone: (555) 123-4567</p>
+              <h4>{JobData.hiring_Managerid?.name}</h4>
+              <p>Email:{JobData.hiring_Managerid?.email}</p>
+              <p>Phone:{JobData.hiring_Managerid?.phonenumber} </p>
             </Card.Body>
           </Card>
           <Card className="mt-4">
@@ -60,12 +78,12 @@ const job = () => {
                 alt="Company Logo"
                 className="img-fluid"
               />
-              <h4>Acme Software Solutions</h4>
-              <p>Location: New York, NY</p>
+              <h4>{JobData.companyid?.name}</h4>
+              <p>Location: {JobData.companyid?.city}, {JobData.companyid?.zipcode}</p>
               <p>
                 Website:{" "}
-                <a href="https://www.acmesoftware.com" target="www.google.com">
-                  www.acmesoftware.com
+                <a href={JobData.companyid?.website} target={JobData.companyid?.website}>
+                {JobData.companyid?.website}
                 </a>
               </p>
             </Card.Body>
@@ -76,4 +94,4 @@ const job = () => {
   );
 };
 
-export default job;
+
